dd666dc80fdb604e9e435a0ff93cb5dcddd31cb6
Stuart Herbert <stuart@stuartherbert.com>
2013-01-29 10:04:25 +0000

    Fix: definition of phpext_xxhash_ptr had trailing semi-colon

--- php_xxhash.h
+++ php_xxhash.h
@@ -4,7 +4,7 @@
 #define PHP_XXHASH_VERSION "1.0.0"
 
 extern const zend_module_entry xxhash_module_entry;
-#define phpext_xxhash_ptr &xxhash_module_entry;
+#define phpext_xxhash_ptr &xxhash_module_entry
 
 #if defined(PHP_WIN32) && defined(XXHASH_EXPORTS)
 #define PHP_XXHASH_API __declspec(dllexport)
