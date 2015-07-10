ea9b9e28020d1d22bf2b4e75db3e74a772982337
Craig R <megasaxon@gmail.com>
2015-07-10 15:27:47 +1000

    Working PHP7 version

--- php_xxhash.c
+++ php_xxhash.c
@@ -10,28 +10,11 @@
 #include "xxhash.c"
 
 #ifdef COMPILE_DL_XXHASH
-ZEND_GET_MODULE(xxhash)
+	ZEND_GET_MODULE(xxhash);
+	#ifdef ZTS
+		ZEND_TSRMLS_CACHE_DEFINE();
+	#endif
 #endif
-
-PHP_MINIT_FUNCTION(xxhash)
-{
-	return SUCCESS;
-}
-
-PHP_MSHUTDOWN_FUNCTION(xxhash)
-{
-	return SUCCESS;
-}
-
-PHP_RINIT_FUNCTION(xxhash)
-{
-	return SUCCESS;
-}
-
-PHP_RSHUTDOWN_FUNCTION(xxhash)
-{
-	return SUCCESS;
-}
 
 PHP_MINFO_FUNCTION(xxhash)
 {
@@ -44,68 +27,62 @@ PHP_MINFO_FUNCTION(xxhash)
 
 PHP_FUNCTION(xxhash32)
 {
-	char *arg1 = NULL;
-	char ret1[128];
-	int arg1_len;
+    char *arg = NULL;
+    size_t arg_len, len;
+    zend_string *strg;
 	unsigned int sum;
 
-	/* parse the parameters */
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &arg1, &arg1_len) == FAILURE || arg1_len < 1)
-	{
-		RETURN_NULL();
-	}
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "s", &arg, &arg_len) == FAILURE || arg_len < 1) {
+        return;
+    }
 
-	/* compute the checksum */
-	sum = XXH32(arg1, arg1_len, 0);
+	// compute the checksum
+	sum = XXH32(arg, arg_len, 0);
 
-	/* convert to hash */
-	sprintf(ret1, "%08x", sum);
+	//convert to a hex string
+	strg = strpprintf(0, "%08x", sum);
 
-	/* return the checksum */
-	RETURN_STRING(ret1, 1);
+	// return the checksum
+	RETURN_STR(strg);
 }
 
 PHP_FUNCTION(xxhash64)
 {
-	char *arg1 = NULL;
-	char ret1[128];
-	int arg1_len;
+	char *arg = NULL;
+    size_t arg_len, len;
+    zend_string *strg;
 	unsigned long long sum;
-	typedef unsigned int U32;
+	//typedef unsigned int U32;
 
-	/* parse the parameters */
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &arg1, &arg1_len) == FAILURE || arg1_len < 1)
-	{
-		RETURN_NULL();
-	}
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "s", &arg, &arg_len) == FAILURE || arg_len < 1) {
+        return;
+    }
 
-	/* compute the checksum */
-	sum = XXH64(arg1, arg1_len, 0);
+	// compute the checksum
+	sum = XXH64(arg, arg_len, 0);
 
-	/* convert to hash */
-	sprintf(ret1, "%08x%08x", (U32)(sum >> 32), (U32)sum);
+	//convert to a hex string
+	strg = strpprintf(0, "%08x%08x", (U32)(sum >> 32), (U32)sum);
 
-	/* return the checksum */
-	RETURN_STRING(ret1, 1);
+	// return the checksum
+	RETURN_STR(strg);
 }
 
-
-zend_function_entry xxhash_functions[] = {
-	PHP_FE(xxhash32, NULL)
-	PHP_FE(xxhash64, NULL)
-	{NULL, NULL, NULL}
+const zend_function_entry xxhash_functions[] = {
+	ZEND_FE(xxhash32, NULL)
+	ZEND_FE(xxhash64, NULL)
+	PHP_FE_END
 };
 
 zend_module_entry xxhash_module_entry = {
 	STANDARD_MODULE_HEADER,
 	"xxhash",
 	xxhash_functions,
-	PHP_MINIT(xxhash),
-	PHP_MSHUTDOWN(xxhash),
+	NULL,
+	NULL,
 	NULL,
 	NULL,
 	PHP_MINFO(xxhash),
 	PHP_XXHASH_VERSION,
 	STANDARD_MODULE_PROPERTIES
 };
-
